a17fbade85d3c03e310ad4148e6ffc1d96274229
shelpii <umbrasasdima@gmail.com>
2024-11-17 23:34:19 -0600

    Implementing a block with differences

---
+++ src/js/modules/difference.js
@@ -0,0 +1,53 @@
+export default class Difference {
+    constructor(oldOfficer, newOfficer, items) {
+        this.oldOfficer = document.querySelector(oldOfficer);
+        this.newOfficer = document.querySelector(newOfficer);
+        this.oldItems = this.oldOfficer.querySelectorAll(items);
+        this.newItems = this.newOfficer.querySelectorAll(items);
+        this.items = items;
+        this.oldCounter = 0;
+        this.newCounter = 0;
+    }
+
+    bindTriggers() {
+        this.oldOfficer.querySelector('.plus').addEventListener('click', () => {
+            if(this.oldCounter !== this.oldItems.length - 2) {
+                this.oldItems[this.oldCounter].style.display = 'flex';
+                this.oldCounter++;
+            } else {
+                this.oldItems[this.oldCounter].style.display = 'flex';
+                this.oldItems[this.oldItems.length - 1].remove();
+            }
+        });
+
+        this.newOfficer.querySelector('.plus').addEventListener('click', () => {
+            if(this.newCounter !== this.newCounter.length - 2) {
+                this.newItems[this.newCounter].style.display = 'flex';
+                this.newCounter++;
+            } else {
+                this.newItems[this.newCounter].style.display = 'flex';
+                this.newItems[this.newItems.length - 1].remove();
+            }
+        });
+    }
+
+    hideItems() {
+        this.oldItems.forEach((item, i, arr) => {
+            if(i !== arr.length - 1) {
+                item.style.display = 'none';
+            }
+        });
+
+        this.newItems.forEach((item, i, arr) => {
+            if(i !== arr.length - 1) {
+                item.style.display = 'none';
+            }
+        });
+    }
+
+    init() {
+        this.hideItems();
+        this.bindTriggers();
+    }
+
+}
